a0bfbb32622bc959b28fac8dca3ca6cdbc6a666a
wuxiaolin <wuxiaolin@gaosiedu.com>
2019-06-22 02:02:27 -0700

    Fix styles.

--- src/pages/news/news.vue
+++ src/pages/news/news.vue
@@ -433,6 +433,7 @@ export default {
     -webkit-line-clamp:2;
     overflow:hidden;
     line-height:36rpx;
+    height: 36upx;
     font-size: 24upx;
   }
   .title-text{
@@ -441,6 +442,8 @@ export default {
     -webkit-line-clamp:1;
     overflow:hidden;
     font-size: 28upx;
+    height: 22upx;
+    line-height: 22upx;
   }
   .selection-content{
     padding-top: 200upx;
